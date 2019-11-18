e00cf5e31f29b31221c80e45bd4a7b8c790a5a57
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-11-17 23:00:50 -0800

    added finalized schema

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -35,6 +35,22 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: payments; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE payments (
+    id uuid NOT NULL,
+    amount integer NOT NULL,
+    description character varying(255),
+    room_occupancy_id uuid NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE payments OWNER TO postgres;
+
+--
 -- Name: properties; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -48,6 +64,41 @@ CREATE TABLE properties (
 
 
 ALTER TABLE properties OWNER TO postgres;
+
+--
+-- Name: room_occupancies; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE room_occupancies (
+    id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    room_id uuid NOT NULL,
+    terminated_at timestamp without time zone NOT NULL,
+    type character varying(255) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE room_occupancies OWNER TO postgres;
+
+--
+-- Name: rooms; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE rooms (
+    id uuid NOT NULL,
+    property_id uuid NOT NULL,
+    name character varying(255) NOT NULL,
+    price_amount integer NOT NULL,
+    payment_schedule character varying(255) NOT NULL,
+    data jsonb NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE rooms OWNER TO postgres;
 
 --
 -- Name: schema_migration; Type: TABLE; Schema: public; Owner: postgres
@@ -97,11 +148,35 @@ CREATE TABLE users (
 ALTER TABLE users OWNER TO postgres;
 
 --
+-- Name: payments payments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY payments
+    ADD CONSTRAINT payments_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: properties properties_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY properties
     ADD CONSTRAINT properties_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: room_occupancies room_occupancies_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY room_occupancies
+    ADD CONSTRAINT room_occupancies_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rooms rooms_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY rooms
+    ADD CONSTRAINT rooms_pkey PRIMARY KEY (id);
 
 
 --
@@ -132,6 +207,38 @@ CREATE UNIQUE INDEX schema_migration_version_idx ON schema_migration USING btree
 --
 
 CREATE INDEX users_email_idx ON users USING btree (email);
+
+
+--
+-- Name: payments payments_room_occupancy_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY payments
+    ADD CONSTRAINT payments_room_occupancy_id_fkey FOREIGN KEY (room_occupancy_id) REFERENCES room_occupancies(id) ON DELETE CASCADE;
+
+
+--
+-- Name: room_occupancies room_occupancies_room_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY room_occupancies
+    ADD CONSTRAINT room_occupancies_room_id_fkey FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE;
+
+
+--
+-- Name: room_occupancies room_occupancies_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY room_occupancies
+    ADD CONSTRAINT room_occupancies_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+
+
+--
+-- Name: rooms rooms_property_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY rooms
+    ADD CONSTRAINT rooms_property_id_fkey FOREIGN KEY (property_id) REFERENCES properties(id) ON DELETE CASCADE;
 
 
 --
