a8597a7fd773a7c0be5a59b7f0585139b8a3ff02
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-11-24 02:09:02 -0800

    added jwt helper so that we can do access token generation locally

---
+++ helpers/jwt.go
@@ -0,0 +1,79 @@
+package helpers
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type RefreshTokenClaim struct {
+	CredentialUUID string `json:"credential_uuid"`
+	SessionUUID    string `json:"session_uuid"`
+	jwt.StandardClaims
+}
+
+type AccessTokenClaim struct {
+	UserID         string `json:"user_id"`
+	CredentialUUID string `json:"credential_uuid"`
+	jwt.StandardClaims
+}
+
+func GenerateAccessToken(userID, credentialUuid string) (string, error) {
+	expirationTime := time.Now().Add(10 * time.Minute)
+	claims := &AccessTokenClaim{
+		UserID:         userID,
+		CredentialUUID: credentialUuid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(secretKey())
+	if err != nil {
+		return "", fmt.Errorf("error exchanging jwt token")
+	}
+
+	return tokenString, nil
+}
+
+func VerifyRefreshToken(tokenString string) (string, error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&RefreshTokenClaim{},
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("There was an error")
+			}
+			return secretKey(), nil
+		})
+	if err != nil {
+		return "", err
+	}
+
+	return token.Claims.(*RefreshTokenClaim).CredentialUUID, nil
+}
+
+func VerifyAccessToken(tokenString string) (string, error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&AccessTokenClaim{},
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("There was an error")
+			}
+			return secretKey(), nil
+		})
+	if err != nil {
+		return "", err
+	}
+
+	return token.Claims.(*AccessTokenClaim).UserID, nil
+}
+
+func secretKey() []byte {
+	return []byte(os.Getenv("SECRET_KEY"))
+}
