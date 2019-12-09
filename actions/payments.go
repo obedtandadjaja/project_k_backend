c0034a558106e5a82b705f3e1b29aa2b4d276fdb
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-12-08 22:57:35 -0800

    added payments to adopt local transactionAndQueryContext pattern

--- actions/payments.go
+++ actions/payments.go
@@ -1,11 +1,11 @@
 package actions
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/validate"
 	"github.com/obedtandadjaja/project_k_backend/models"
 )
 
@@ -13,20 +13,28 @@ type PaymentsResource struct {
 	buffalo.Resource
 }
 
+func (v PaymentsResource) getTransactionAndQueryContext(c buffalo.Context) (*pop.Connection, *pop.Query) {
+	tx, _ := c.Value("tx").(*pop.Connection)
+
+	return tx, tx.Q().
+		InnerJoin("room_occupancies", "room_occupancies.id = payments.room_occupancy_id").
+		InnerJoin("rooms", "rooms.id = room_occupancies.room_id").
+		InnerJoin("properties", "properties.id = rooms.property_id").
+		InnerJoin("user_property_relationships", "user_property_relationships.property_id = properties.id").
+		Where("room_occupancies.user_id = ?", c.Param("tenant_id")).
+		Where("rooms.id = ?", c.Param("room_id")).
+		Where("properties.id = ?", c.Param("property_id")).
+		Where("user_property_relationships.user_id = ?", c.Value("current_user_id"))
+}
+
 func (v PaymentsResource) List(c buffalo.Context) error {
-	// Get the DB connection from the context
-	tx, ok := c.Value("tx").(*pop.Connection)
-	if !ok {
-		return fmt.Errorf("no transaction found")
-	}
+	_, q := v.getTransactionAndQueryContext(c)
 
 	payments := &models.Payments{}
 
 	// Paginate results. Params "page" and "per_page" control pagination.
 	// Default values are "page=1" and "per_page=20".
-	q := tx.PaginateFromParams(c.Params())
-
-	// Retrieve all Payments from the DB
+	q = q.PaginateFromParams(c.Params())
 	if err := q.All(payments); err != nil {
 		return err
 	}
@@ -38,21 +46,9 @@ func (v PaymentsResource) List(c buffalo.Context) error {
 }
 
 func (v PaymentsResource) Show(c buffalo.Context) error {
-	tx, ok := c.Value("tx").(*pop.Connection)
-	if !ok {
-		return fmt.Errorf("no transaction found")
-	}
+	_, q := v.getTransactionAndQueryContext(c)
 
 	payment := &models.Payment{}
-	q := tx.Q().
-		InnerJoin("room_occupancies", "room_occupancies.id = payments.room_occupancy_id").
-		InnerJoin("rooms", "rooms.id = room_occupancies.room_id").
-		InnerJoin("properties", "properties.id = rooms.property_id").
-		InnerJoin("user_property_relationships", "user_property_relationships.property_id = properties.id").
-		Where("room_occupancies.user_id = ?", c.Param("tenant_id")).
-		Where("rooms.id = ?", c.Param("room_id")).
-		Where("properties.id = ?", c.Param("property_id")).
-		Where("user_property_relationships.user_id = ?", c.Value("current_user_id"))
 	if err := q.Find(payment, c.Param("payment_id")); err != nil {
 		return c.Error(http.StatusNotFound, err)
 	}
@@ -62,14 +58,18 @@ func (v PaymentsResource) Show(c buffalo.Context) error {
 
 func (v PaymentsResource) Create(c buffalo.Context) error {
 	payment := &models.Payment{}
-
 	if err := c.Bind(payment); err != nil {
 		return err
 	}
 
-	tx, ok := c.Value("tx").(*pop.Connection)
-	if !ok {
-		return fmt.Errorf("no transaction found")
+	tx, q := v.getTransactionAndQueryContext(c)
+
+	// Check that user has ownership of tenant and that tenant exists
+	err := q.First(&models.RoomOccupancy{})
+	if err != nil {
+		verrs := validate.NewErrors()
+		verrs.Add("property", "Room occupancy does not exist")
+		return c.Render(http.StatusUnprocessableEntity, r.JSON(verrs))
 	}
 
 	verrs, err := tx.ValidateAndCreate(payment)
@@ -86,14 +86,11 @@ func (v PaymentsResource) Create(c buffalo.Context) error {
 }
 
 func (v PaymentsResource) Update(c buffalo.Context) error {
-	tx, ok := c.Value("tx").(*pop.Connection)
-	if !ok {
-		return fmt.Errorf("no transaction found")
-	}
+	tx, q := v.getTransactionAndQueryContext(c)
 
 	payment := &models.Payment{}
 
-	if err := tx.Find(payment, c.Param("payment_id")); err != nil {
+	if err := q.Find(payment, c.Param("payment_id")); err != nil {
 		return c.Error(http.StatusNotFound, err)
 	}
 
@@ -115,14 +112,11 @@ func (v PaymentsResource) Update(c buffalo.Context) error {
 }
 
 func (v PaymentsResource) Destroy(c buffalo.Context) error {
-	tx, ok := c.Value("tx").(*pop.Connection)
-	if !ok {
-		return fmt.Errorf("no transaction found")
-	}
+	tx, q := v.getTransactionAndQueryContext(c)
 
 	payment := &models.Payment{}
 
-	if err := tx.Find(payment, c.Param("payment_id")); err != nil {
+	if err := q.Find(payment, c.Param("payment_id")); err != nil {
 		return c.Error(http.StatusNotFound, err)
 	}
 
