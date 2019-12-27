618c00e6c232bb6eedc48d405e6b2d1216a69ae8
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-12-26 22:47:28 -0800

    initial user test

--- actions/users_test.go
+++ actions/users_test.go
@@ -1,16 +1,30 @@
 package actions
 
-func (as *ActionSuite) Test_UsersResource_List() {
-}
+import (
+	"encoding/json"
+
+	"github.com/obedtandadjaja/project_k_backend/models"
+)
 
 func (as *ActionSuite) Test_UsersResource_Show() {
-}
+	as.LoadFixture("user")
+	res := as.JSON("/api/v1/users/%s", "user").Get()
+	as.Equal(200, res.Code)
 
-func (as *ActionSuite) Test_UsersResource_Create() {
+	var responseBody map[string]interface{}
+	json.NewDecoder(res.Body).Decode(responseBody)
+
+	as.Equal(responseBody["id"], "user")
+	as.Equal(responseBody["email"], "user@example.com")
 }
 
 func (as *ActionSuite) Test_UsersResource_Update() {
-}
+	as.LoadFixture("user")
+	res := as.JSON("/api/v1/users/%s", "user").Put(&models.User{Email: "asdf@example.com"})
+	as.Equal(200, res.Code)
 
-func (as *ActionSuite) Test_UsersResource_Destroy() {
+	user := &models.User{}
+	as.DB.Find(user, "user")
+
+	as.Equal(user.Email, "asdf@example.com")
 }
