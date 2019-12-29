14fa54d1883d2a2090ea65b219eaace300956529
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-12-28 20:33:55 -0800

    fixed user tests

--- actions/users_test.go
+++ actions/users_test.go
@@ -2,29 +2,60 @@ package actions
 
 import (
 	"encoding/json"
+	"fmt"
 
+	"github.com/gobuffalo/suite/fix"
+	"github.com/obedtandadjaja/project_k_backend/helpers"
 	"github.com/obedtandadjaja/project_k_backend/models"
 )
 
 func (as *ActionSuite) Test_UsersResource_Show() {
 	as.LoadFixture("user")
-	res := as.JSON("/api/v1/users/%s", "user").Get()
+
+	fixture, err := fix.Find("user")
+	userID := fixture.Tables[0].Row[0]["id"]
+	credentialUUID := fixture.Tables[0].Row[0]["credential_uuid"]
+
+	token, err := helpers.GenerateAccessToken(userID.(string), credentialUUID.(string))
+	if err != nil {
+		as.NoError(err)
+	}
+
+	req := as.JSON("/api/v1/users/%s", userID.(string))
+	req.Headers = map[string]string{
+		"Authorization": token,
+	}
+	res := req.Get()
 	as.Equal(200, res.Code)
 
 	var responseBody map[string]interface{}
-	json.NewDecoder(res.Body).Decode(responseBody)
+	json.NewDecoder(res.Body).Decode(&responseBody)
 
-	as.Equal(responseBody["id"], "user")
+	as.Equal(responseBody["id"], userID.(string))
 	as.Equal(responseBody["email"], "user@example.com")
 }
 
 func (as *ActionSuite) Test_UsersResource_Update() {
 	as.LoadFixture("user")
-	res := as.JSON("/api/v1/users/%s", "user").Put(&models.User{Email: "asdf@example.com"})
+
+	fixture, err := fix.Find("user")
+	userID := fixture.Tables[0].Row[0]["id"]
+	credentialUUID := fixture.Tables[0].Row[0]["credential_uuid"]
+
+	token, err := helpers.GenerateAccessToken(userID.(string), credentialUUID.(string))
+	if err != nil {
+		as.NoError(err)
+	}
+
+	req := as.JSON("/api/v1/users/%s", userID.(string))
+	req.Headers = map[string]string{
+		"Authorization": token,
+	}
+	res := req.Put(&models.User{ID: helpers.ParseUUID(userID.(string)), Email: "asdf@example.com"})
 	as.Equal(200, res.Code)
 
 	user := &models.User{}
-	as.DB.Find(user, "user")
+	as.DB.Find(user, userID.(string))
 
-	as.Equal(user.Email, "asdf@example.com")
+	as.Equal("asdf@example.com", user.Email)
 }
