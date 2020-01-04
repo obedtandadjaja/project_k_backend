bcf32e5f3cbbe0c9f0c9c4e7aa5de8b38f24ef87
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2020-01-04 14:59:59 -0800

    added at least one validator

---
+++ helpers/validators/at_least_one.go
@@ -0,0 +1,25 @@
+package validators
+
+import (
+	"fmt"
+
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
+)
+
+type AtLeastOne struct {
+	Name       string
+	Validators []validate.Validator
+	Message    string
+}
+
+func (v *AtLeastOne) IsValid(errors *validate.Errors) {
+	for _, validator := range v.Validators {
+		newErrors := &validate.Errors{}
+		if validator.IsValid(newErrors); !newErrors.HasAny() {
+			return
+		}
+	}
+
+	errors.Add(validators.GenerateKey(v.Name), fmt.Sprintf("None of the validators pass"))
+}
